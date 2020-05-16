14ae02b093e9b28c7c799e825135122408941cb3
Ivan Hudzynskyi <ivanhudzinskyi2000@gmail.com>
2020-05-17 01:49:06 +0300

    Make repo names clickable, store repos data

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ var tbUser = document.getElementById("user");
 var userRepoUrl = "https://api.github.com/users/{user}/repos";
 var repoCommitsUrl = "https://api.github.com/repos/{user}/{repo}/commits";
 
+var reposData = [];
+
 btnGetRepos.addEventListener("click", loadRepos);
 
 async function loadRepos() {
     let user = tbUser.value;
 
     if (user == "") {
-        alert("Please specify the usrname for the GitHub account!");
+        alert("Please specify the username for the GitHub account!");
         return;
     }
 
@@ -21,14 +23,20 @@ async function loadRepos() {
 
     let url = userRepoUrl.replace("{user}", user);
 
-    let repoData = await (await fetch(url)).json();
+    reposData = await (await fetch(url)).json();
 
     ulRepos.innerHTML = "";
 
-    repoData.forEach(repo => {
+    for (let i = 0; i < reposData.length; i++) {
         let li = document.createElement("li");
-        li.innerHTML = repo.name;
+        li.innerHTML = reposData[i].name;
+
+        li.addEventListener("click", () => loadRepoData(i));
 
         ulRepos.appendChild(li);
-    });
+    }
+}
+
+function loadRepoData(index) {
+    alert(reposData[index].name);
 }
